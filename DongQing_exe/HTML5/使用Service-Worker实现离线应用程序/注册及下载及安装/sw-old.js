40554c0892d43e51542b18bce505ba5cc50ec87d
DongQing <718679046@qq.com>
2025-07-27 10:12:41 +0800

    Service Worker示例完善

--- DongQing_exe/HTML5/使用Service-Worker实现离线应用程序/注册及下载及安装/sw-old.js
+++ DongQing_exe/HTML5/使用Service-Worker实现离线应用程序/注册及下载及安装/sw-old.js
@@ -2,47 +2,19 @@
 self.addEventListener("install",function(event){
     //执行安装过程
     event.waitUntil(
-        caches.open('v2').then(function(cache) {
+        caches.open('v1').then(function(cache) {
             //缓存我们想要缓存的文件
             return cache.addAll([
-                 '/',
-                 '/index.html',
-                 '/styles/main.css'
+                '/',
+                '/index.html',
+                '/styles/main.css',
+                '/script/main.js',
+                '/fallback.html'
             ]);
-        }).then(function(){
-            return caches.open('v3').then(function(cache) {
-                //缓存我们想要缓存的文件
-                return cache.addAll([
-                     '/script/main.js',
-                     '/fallback.html'
-                ]);
-            });
-       }).then(function() {
+        }).then(function() {
                 console.log('所有资源被成功缓存');
             }).catch(function(error) {
             console.log('预抓取失败:', error);
-        })
-    );
-});
-
-self.addEventListener("activate",function(event){
-    let cacheWhitelist=["v2","v3"];
-    event.waitUntil(
-        caches.keys().then(function(cacheNames){
-            return Promise.all(
-                cacheNames.map(function(cacheName){
-                    if(cacheWhitelist.indexOf(cacheName)===-1){
-                        console.log(cacheName+"缓存被删除");
-                        return caches.delete(cacheName);
-                    }
-                    // 添加返回值以确保Promise.all正常工作
-                    return Promise.resolve();
-                })
-            );
-        }).then(function() {
-            console.log('缓存清理完成');
-        }).catch(function(error) {
-            console.log('缓存清理失败:', error);
         })
     );
 });
@@ -64,7 +36,7 @@ self.addEventListener('fetch', function(event) {
                 }
                 
                 // 如果是有效响应，将其添加到缓存并返回
-                return caches.open('v2').then(function(cache) {
+                return caches.open('v1').then(function(cache) {
                     cache.put(event.request, response.clone());
                     return response;
                 });
